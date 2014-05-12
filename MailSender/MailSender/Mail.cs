161468dc1118e0ac5db90d1993ced90b7b0225cf
Chrystian Kisło <chrystian.kislo@gmail.com>
2014-05-12 20:39:15 +0200

    Let's go PoKeMoN'S

---
+++ MailSender/MailSender/Mail.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender
+{
+    class Mail
+    {
+        private List<String> tos;
+        private String content;
+        private String subject;
+        private bool html;
+
+
+        public Mail(String to, String subject, String content = "", bool html = false) : this(subject, content, html)
+        {
+            tos.Add(to);
+        }
+
+        public Mail(String subject, String content = "", bool html = false)
+        {
+            this.subject = subject;
+            this.content = content;
+            this.html = html;
+        }
+        public bool AddRecipient(String recipient)
+        {
+            return true;
+        }
+        public bool AddAtachment()
+        {
+            return true;
+        }
+    }
+}
---
+++ MailSender/MailSender/MailSenderInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender
+{
+    public interface MailSenderInterface
+    {
+        public MailSenderInterface(string apikey);
+        public bool SendMail(Mail mail);
+        public bool SendTestMail();
+        public bool TestKey();
+    }
+}
